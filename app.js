17ef8edff2160717836ec82334e3f239bdb0d217
jningtho <jningtho@x-designs.net>
2017-12-29 14:53:20 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//console.log("Hello Blockchain Developers!")
+const express = require('express')
+const app = express();
+const Web3 = require('web3');
+const web3 = new Web3('http://localhost:8545');
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+})
+
+// create route ajax-request
+app.get('/ajax-request', (req, res) => {
+    web3.eth.getAccounts(function(error, addresses){
+      if(error == null) res.send(JSON.stringify(addresses))
+    })
+})
+
+
+app.listen(3000);
